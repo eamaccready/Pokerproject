336b50f629083b03c82b149263e8ae9538e8182f
eamaccready <eamaccready@gmail.com>
2016-11-14 21:47:15 -0800

    fixed switch statement in Card class.

--- Poker/Program.cs
+++ Poker/Program.cs
@@ -8,42 +8,54 @@ using System.Threading.Tasks;
 //dgarber@microsoft.com for help!
 namespace Poker
 {
-    class Card :IComparable
+    class Card : IComparable
     {
         public char suit; //CHDS 
         public int rank;  // 2=2, K=13, A = 14
 
-        public Card()
+        public Card()// why is method overloaded? What should this do?
         {
 
         }
 
-        public Card(string str)//gets vals from deal? I don't fully understand what this is doing...
+        public Card(string str)//method to process cardtype input.
         {
             str = str.ToUpper();// converts all input to upper case.
             foreach (char c in str)
             {
-                switch(c)
+                switch (c) // is this the best way to handle?
                 {
                     case 'C':
+                    case 'D':
+                    case 'H':
+                    case 'S':
                         suit = c;
                         break;
-                    case 'D':
-                        break;
-                    case 'H':
-                        break;
-                    case 'S':
-                        break;
                     case 'A':
+                        rank = 14;
                         break;
                     case 'K':
+                        rank = 13;
                         break;
                     case 'Q':
+                        rank = 12;
                         break;
                     case 'J':
+                        rank = 11;
                         break;
-                    case 'T':
-                        break;                    
+                    case 'T':// should I make a case for T and 10? or is string only taking 2 spots as input?
+                        rank = 10;
+                        break;
+                    case '9':
+                    case '8':
+                    case '7':
+                    case '6':
+                    case '5':
+                    case '4':
+                    case '3':
+                    case '2':
+                        rank = c - '0'; //idiom to get int value, I think...
+                        break;
                 }
             }
 
@@ -53,7 +65,7 @@ namespace Poker
 
         bool isValid()
         {
-            return suit != '\0'  && rank >= 2 && rank <=14;
+            return suit != '\0' && rank >= 2 && rank <= 14;
         }
 
         public int CompareTo(object obj)
@@ -61,17 +73,17 @@ namespace Poker
             Card c = obj as Card;
             return rank - c.rank;
         }
- }
+    }
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            Card[] hand = GetHand(args);//eventually
+            Card[] hand = GetHand(args);//eventually class = [] + dict.
 
             //Card[] hand = new Card[5];
-           // hand[0] = new Card();
+            // hand[0] = new Card();
             //hand[0].suit = 'C';
             //hand[0].rank = 8;
             //....
@@ -79,97 +91,97 @@ namespace Poker
 
             //Prints hand to end user in console.
             if (isStraightFlush(hand))
-                Console.WriteLine('Straight flush!');
+                Console.WriteLine("Straight flush!");
             else if (isFourOfKind(hand))
-                Console.WriteLine('Four of a kind!');
+                Console.WriteLine("Four of a kind!");
             else if (isFullHouse(hand))
-                Console.WriteLine('Full house!');
+                Console.WriteLine("Full house!");
             else if (isFlush(hand))
-                Console.WriteLine('Flush!');
+                Console.WriteLine("Flush!");
             else if (isStraight(hand))
-                Console.WriteLine('Straight!');
+                Console.WriteLine("Straight!");
             else if (isThreeOfKind(hand))
-                Console.WriteLine('Three of a kind!');
+                Console.WriteLine("Three of a kind!");
             else if (isTwoPair(hand))
-                Console.WriteLine('Two pair!'); // also print pairs?
+                Console.WriteLine("Two pair!"); // also print pairs?
             else if (isPair(hand))
-                Console.WriteLine('A pair!') //also print value of pair?
+                Console.WriteLine("A pair!"); //also print value of pair?
             else
-                Console.WriteLine('Your highest card is: ' + hand[5]); // print last value of array since is sorted.
+                Console.WriteLine("Your highest card is: " + hand[5]); // print last value of array since is sorted.
         }
 
-            static Card[] GetHand(string[] args)
+        static Card[] GetHand(string[] args)
+        {
+            Card[] hand = new Card[5];
+            int index = 0;
+            foreach (string a in args)
             {
-                Card[] hand = new Card[5];
-                int index = 0;
-                foreach(string a in args)
-                {
-                    if (index >= 5)
-                        break;
-                    Card c = new Card(a)
-                    hand[index++] = c;
-                }
+                if (index >= 5)
+                    break;
+                Card c = new Card(a);
+                hand[index++] = c;
+            }
 
-                while (index < 5) 
-                {
-                    hand[index++] = Deal();
-                }
-                return hand;
+            while (index < 5)
+            {
+                hand[index++] = Deal();
+            }
+            return hand;
         }
 
 
         // create a deck and deal a random card.
         static Card[] deck = null;
-        static int dealIndex =0;
+        static int dealIndex = 0;
 
         static Card Deal()
         {
-            if (deck == null | dealIndex >=52)
-            { 
+            if (deck == null | dealIndex >= 52)
+            {
                 deck = new Card[52];
-                int index == 0;
+                int index = 0;
                 // todo : one of each card == loop!
                 //shuffle (randomize)
             }
             return deck[dealIndex++];
         } //end class program.
 
-         // function to evaluate if hand is straight flush.
-       static bool isStraightFlush(Card[] hand)
-       {
-            return isStraightFlush(hand) && isStraightFlush(hand);
-       }
-
-            static bool isFlush(Card[] hand)
+        // function to evaluate if hand is straight flush.
+        static bool isStraightFlush(Card[] hand)
         {
-                for (int i = 1; i < hand.Length; i++)
-                {
-                    if (hand[i].suit != hand[0].suit)
-                        return false;
-                }
-                return true;
-            }
+            return isStraightFlush(hand) && isStraightFlush(hand);
+        }
 
-            static bool isStraight(Card[] hand)//Assuming sorted.
+        static bool isFlush(Card[] hand)
+        {
+            for (int i = 1; i < hand.Length; i++)
             {
-                for (int i = 1; i < hand.Length; i++)
-                {
-                    if (hand[i].rank != hand[i - 1].rank + 1)
-                        return false;
-                }
-                return true;
+                if (hand[i].suit != hand[0].suit)
+                    return false;
             }
-        
-         static void cardCounts(Card[] hand)// Maybe void? I want counts of each rank.
-         {
-         // make dictionary of all ranks so you can get counts.
-         Dictionary<string,int> 
-         }
+            return true;
+        }
+
+        static bool isStraight(Card[] hand)//Assuming sorted.
+        {
+            for (int i = 1; i < hand.Length; i++)
+            {
+                if (hand[i].rank != hand[i - 1].rank + 1)
+                    return false;
+            }
+            return true;
+        }
+
+        static void cardCounts(Card[] hand)// Maybe void? I want counts of each rank.
+        {
+            // make dictionary of all ranks so you can get counts.
+            Dictionary<string, int>;
+        }
 
         // 
         static bool isFullhouse(Card[] hand)
         {
-            //if cardMatches counts 3==true && 2==true. return true.
+            //if cardCounts counts 3==true && 2==true. return true.
             return true;
         }
 
@@ -189,6 +201,7 @@ namespace Poker
             //if cardMatches counts == 2 * 1. return true 
             return true;
         }
+    }
 }
 
 // if counts == 3 && counts ==2; return is Full house
